a1361f3bc4426280b10aac10c06bc60bde7ed976
omardelarosa <thedelarosa@gmail.com>
2015-11-01 09:36:46 -0500

    Updated default port to 5010

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ module.exports.start = function(done){
   // bind routes
   routes(app);
 
-  var port = Number(process.env.PORT || 5001);
+  var port = Number(process.env.PORT || 5010);
 
   var server = app.listen(port, function(){
     console.log('Listening on port ' + port);
