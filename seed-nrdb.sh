cca8e06fc0e552feb2785941aabec47080eb5374
omardelarosa <thedelarosa@gmail.com>
2015-11-02 16:24:22 +0000

    Fix seed script

--- seed-nrdb.sh
+++ seed-nrdb.sh
@@ -1,3 +1,3 @@
-
+cd $HOME/netrunner-search-api
 echo "[$(date)]: $(curl -XDELETE http://localhost:9200/netrunner)" >> $HOME/nrdb-imports.log
 echo "[$(date)]: $($HOME/netrunner-search-api/node_modules/.bin/gulp importCards)" >> $HOME/nrdb-imports.log
